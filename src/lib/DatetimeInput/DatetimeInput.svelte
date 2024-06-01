5b537d86a9d825aac059ebe753b7aa725352a74c
Acylation <532117255@qq.com>
2024-06-02 04:23:32 +0800

    Add datetime input

---
+++ src/lib/DatetimeInput/DatetimeInput.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import dayjs from "dayjs";
+
+	/**
+	 * Specifies the date time value.
+	 */
+	export let value: Date | null;
+
+	/**
+	 * Specifies whether to remove decorations so that it can be embedded in other
+	 * components.
+	 */
+	export let embed: boolean = false;
+
+	const dispatch = createEventDispatcher<{ change: Date | null }>();
+
+	function handleChange(event: Event) {
+		if (event.currentTarget instanceof HTMLInputElement) {
+			dispatch(
+				"change",
+				event.currentTarget.value
+					? dayjs(event.currentTarget.value).toDate()
+					: null,
+			);
+		}
+	}
+</script>
+
+<input
+	type="datetime-local"
+	class:embed
+	value={value ? dayjs(value).format("YYYY-MM-DDHH:mm") : null}
+	max="9999-12-31T23:59"
+	on:change={handleChange}
+	on:blur
+/>
+
+<style>
+	input {
+		border-radius: 9999px;
+		border: 0;
+		background-color: var(--background-modifier-hover);
+		font-family: var(--font-default);
+		padding: 0.1em 0.6em;
+	}
+
+	.embed {
+		margin: 0 8px;
+	}
+</style>
